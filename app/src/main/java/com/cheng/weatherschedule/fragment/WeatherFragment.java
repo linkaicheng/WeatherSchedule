935c1fd4c2daef0f46f2804314e57189b16f60df
linkaicheng <1063002901@qq.com>
2016-09-07 00:47:59 +0800

    重新编写了界面

--- app/src/main/java/com/cheng/weatherschedule/fragment/WeatherFragment.java
+++ app/src/main/java/com/cheng/weatherschedule/fragment/WeatherFragment.java
@@ -9,16 +9,13 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
 import android.widget.ImageView;
-import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.cheng.weatherschedule.R;
 import com.cheng.weatherschedule.bean.LifeSuggestion;
 import com.cheng.weatherschedule.bean.WeatherDaily;
-import com.cheng.weatherschedule.bean.WeatherViewHolder;
 import com.cheng.weatherschedule.utils.URLConnManager;
 import com.google.gson.Gson;
 import com.google.gson.JsonSyntaxException;
@@ -35,17 +32,21 @@ import java.util.Map;
  * Created by cheng on 2016/9/5.
  */
 public class WeatherFragment extends Fragment {
-    private ListView lvWeather;
-    private Adapter adapter;
-    private List<Map<String,Object>> data=null;
+
+    private List<Map<String, Object>> data = null;
     private ProgressDialog pDialog;
     private String action;
     private TextView tvCity;
+    private ImageView imWeather, imWeatherToday, imWeatherTomorrow, imWeaAfterTomo;
+    private TextView tvWeather, tvTemp, tvWindDirection, tvUv, tvWindScale
+            , tvTempToday, tvTextToday, tvTempTomorrow, tvTextTomorrow
+            , tvTempAfterTomo, tvTextAfterTomo,tvDate;
+
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_weather,container,false);
+        return inflater.inflate(R.layout.fragment_weather, container, false);
     }
 
     @Override
@@ -53,20 +54,35 @@ public class WeatherFragment extends Fragment {
         super.onActivityCreated(savedInstanceState);
         initView();
     }
-private void initView(){
-    lvWeather = (ListView) getActivity().findViewById(R.id.lvWeather);
-    tvCity = (TextView) getActivity().findViewById(R.id.tvCity);
-    data=new ArrayList<>();
-    adapter=new Adapter(data);
-    lvWeather.setAdapter(adapter);
-    String url="https://api.thinkpage.cn/v3/weather/daily.json?key=r3b44bu4dzqzadlr&location=beijing&language=zh-Hans&unit=c&start=0&days=5";
-    action="daily";
-    new WeatherTask().execute(url);
 
-}
-    private class WeatherTask extends AsyncTask<String,Void,Object>{
+    private void initView() {
+        tvCity = (TextView) getActivity().findViewById(R.id.tvCity);
+        imWeather = (ImageView) getActivity().findViewById(R.id.imWeather);
+        imWeatherToday = (ImageView) getActivity().findViewById(R.id.imWeatherToday);
+        imWeatherTomorrow = (ImageView) getActivity().findViewById(R.id.imWeatherTomorrow);
+        imWeaAfterTomo = (ImageView) getActivity().findViewById(R.id.imWeaAfterTomo);
 
+        tvWeather = (TextView) getActivity().findViewById(R.id.tvWeather);
+        tvTemp = (TextView) getActivity().findViewById(R.id.tvTemp);
+        tvWindDirection = (TextView) getActivity().findViewById(R.id.tvWindDirection);
+        tvUv = (TextView) getActivity().findViewById(R.id.tvUv);
+        tvWindScale = (TextView) getActivity().findViewById(R.id.tvWindScale);
+        tvTempToday = (TextView) getActivity().findViewById(R.id.tvTempToday);
+        tvTextToday = (TextView) getActivity().findViewById(R.id.tvTextToday);
+        tvTempTomorrow = (TextView) getActivity().findViewById(R.id.tvTempTomorrow);
+        tvTextTomorrow = (TextView) getActivity().findViewById(R.id.tvTextTomorrow);
+        tvTempAfterTomo = (TextView) getActivity().findViewById(R.id.tvTempAfterTomo);
+        tvTextAfterTomo = (TextView) getActivity().findViewById(R.id.tvTextAfterTomo);
+        tvDate = (TextView) getActivity().findViewById(R.id.tvDate);
 
+        data = new ArrayList<>();
+        String url = "https://api.thinkpage.cn/v3/weather/daily.json?key=r3b44bu4dzqzadlr&location=beijing&language=zh-Hans&unit=c&start=0&days=5";
+        action = "daily";
+        new WeatherTask().execute(url);
+
+    }
+
+    private class WeatherTask extends AsyncTask<String, Void, Object> {
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
@@ -76,54 +92,52 @@ private void initView(){
 
         @Override
         protected Object doInBackground(String... params) {
-            String result=null;
-            InputStream inputStream=null;
-            HttpURLConnection conn=null;
-            String url=params[0];
+            String result = null;
+            InputStream inputStream = null;
+            HttpURLConnection conn = null;
+            String url = params[0];
             try {
                 //获取连接
                 conn = URLConnManager.getHttpURLConnection(url);
                 //连接
                 conn.connect();
                 //获得响应码
-                int code=conn.getResponseCode();
+                int code = conn.getResponseCode();
                 //Log.e("cheng","***********"+code);
-                if(code==200){//连接成功
+                if (code == 200) {//连接成功
                     //获取服务器返回的数据
-                    inputStream=conn.getInputStream();
+                    inputStream = conn.getInputStream();
                     //将输入流转成字符串
-                    String response=URLConnManager.converStreamToString(inputStream);
-                   // Log.e("cheng","************"+response);
+                    String response = URLConnManager.converStreamToString(inputStream);
+                    // Log.e("cheng","************"+response);
                     //用Gson解析数据
-                    Gson gson=new Gson();
-                    if(action.equals("daily")){
-                        WeatherDaily weatherDaily=gson.fromJson(response,WeatherDaily.class);
+                    Gson gson = new Gson();
+                    if (action.equals("daily")) {
+                        WeatherDaily weatherDaily = gson.fromJson(response, WeatherDaily.class);
                         return weatherDaily;
-                    }else if(action.equals("suggestion")){
-                        LifeSuggestion lifeSuggestion=gson.fromJson(response,LifeSuggestion.class);
+                    } else if (action.equals("suggestion")) {
+                        LifeSuggestion lifeSuggestion = gson.fromJson(response, LifeSuggestion.class);
                         return lifeSuggestion;
                     }
-                }else{//连接失败
-                    result="2";
+                } else {//连接失败
+                    result = "2";
                 }
-
             } catch (IOException e) {
                 e.printStackTrace();
                 result = "2";
-            }catch (JsonSyntaxException e){
+            } catch (JsonSyntaxException e) {
                 e.printStackTrace();
-                result="3";
-            }
-            finally {
+                result = "3";
+            } finally {
                 //关闭输入流和连接等资源
-                if(inputStream!=null){
+                if (inputStream != null) {
                     try {
                         inputStream.close();
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
                 }
-                if(conn!=null){
+                if (conn != null) {
                     conn.disconnect();
                 }
             }
@@ -135,140 +149,90 @@ private void initView(){
         protected void onPostExecute(Object result) {
             super.onPostExecute(result);
 
-            LifeSuggestion lifeSuggestion=null;
-            WeatherDaily weatherDaily=null;
+            LifeSuggestion lifeSuggestion = null;
+            WeatherDaily weatherDaily = null;
             //关闭进度对话框
-            if(pDialog!=null){
+            if (pDialog != null) {
                 pDialog.dismiss();
-
             }
-
-            if(result instanceof WeatherDaily){
-                weatherDaily=(WeatherDaily)result;
-                if(weatherDaily==null){
+            if (result instanceof WeatherDaily) {
+                weatherDaily = (WeatherDaily) result;
+                if (weatherDaily == null) {
                     Toast.makeText(getActivity(), "没有查询到相关的天气信息", Toast.LENGTH_SHORT).show();
                     //查询到空，将数据清空，更新界面
-                    data.clear();
-                    adapter.notifyDataSetChanged();
-                }else{
+                } else {
                     tvCity.setText(weatherDaily.getResults().get(0).getLocation().getName());
                     // 往data中填充数据
-                    for(WeatherDaily.ResultsBean.DailyBean daily:weatherDaily.getResults().get(0).getDaily()){
-                        Map<String,Object> row=new HashMap<>();
-                        row.put("date",daily.getDate());
-                        row.put("codeDay",daily.getCode_day());
-                        row.put("temp",daily.getLow()+"-"+daily.getHigh());
-                        row.put("textDay",daily.getText_day());
-                        row.put("direction",daily.getWind_direction());
+                    for (WeatherDaily.ResultsBean.DailyBean daily : weatherDaily.getResults().get(0).getDaily()) {
+                        Map<String, Object> row = new HashMap<>();
+                        row.put("date", daily.getDate());
+                        row.put("codeDay", daily.getCode_day());
+                        row.put("temp", daily.getLow() + "-" + daily.getHigh());
+                        row.put("textDay", daily.getText_day());
+                        row.put("direction", daily.getWind_direction());
+                        row.put("windScale", daily.getWind_scale());
                         data.add(row);
                     }
-                   String url="https://api.thinkpage.cn/v3/life/suggestion.json?key=r3b44bu4dzqzadlr&location=beijing&language=zh-Hans";
-                    action="suggestion";
+                    String url = "https://api.thinkpage.cn/v3/life/suggestion.json?key=r3b44bu4dzqzadlr&location=beijing&language=zh-Hans";
+                    action = "suggestion";
                     new WeatherTask().execute(url);
 
                 }
-            }else if(result instanceof LifeSuggestion){
-                lifeSuggestion=(LifeSuggestion)result;
-                LifeSuggestion.ResultsBean.SuggestionBean suggestionBean=lifeSuggestion.getResults().get(0).getSuggestion();
-                String suggestion=suggestionBean.getCar_washing().getBrief()+"洗车;"
-                                    +suggestionBean.getSport().getBrief()+"运动;"
-                                    +suggestionBean.getTravel().getBrief()+"旅游;"
-                                    +suggestionBean.getFlu().getBrief()+"感冒.";
-                if(data.size()!=0){
-                    for(int i=0;i<data.size();i++){
-                         data.get(i).put("uv","紫外线："+suggestionBean.getUv().getBrief());
-                        data.get(i).put("suggestion",suggestion);
+            } else if (result instanceof LifeSuggestion) {
+                lifeSuggestion = (LifeSuggestion) result;
+                LifeSuggestion.ResultsBean.SuggestionBean suggestionBean = lifeSuggestion.getResults().get(0).getSuggestion();
+//                String suggestion=suggestionBean.getCar_washing().getBrief()+"洗车;"
+//                                    +suggestionBean.getSport().getBrief()+"运动;"
+//                                    +suggestionBean.getTravel().getBrief()+"旅游;"
+//                                    +suggestionBean.getFlu().getBrief()+"感冒.";
+                if (data.size() != 0) {
+                    for (int i = 0; i < data.size(); i++) {
+                        data.get(i).put("uv", "紫外线：" + suggestionBean.getUv().getBrief());
+                        //data.get(i).put("suggestion",suggestion);
                     }
-                    adapter.notifyDataSetChanged();
                 }
+                Map<String, Object> day1 = data.get(0);
+                Map<String, Object> day2 = data.get(1);
+                Map<String, Object> day3 = data.get(2);
+                int resId=getResource("weather"+day1.get("codeDay"));
+                imWeather.setImageResource(resId);
+                tvWeather.setText((String)day1.get("textDay"));
+                tvTemp.setText((String)day1.get("temp"));
+                tvWindDirection.setText((String)day1.get("direction"));
+                tvDate.setText((String)day1.get("date"));
+                tvUv.setText((String)day1.get("uv"));
+                tvWindScale.setText("风力等级："+day1.get("windScale")+"级");
+                resId=getResource("s"+day1.get("codeDay"));
+                imWeatherToday.setImageResource(resId);
+                resId=getResource("s"+day2.get("codeDay"));
+                imWeatherTomorrow.setImageResource(resId);
+                resId=getResource("s"+day3.get("codeDay"));
+                imWeaAfterTomo.setImageResource(resId);
 
-            }
+                tvTempToday.setText((String)day1.get("temp"));
+                tvTextToday.setText((String)day1.get("textDay"));
 
-            else if(result instanceof String){
-                if("2".equals(result)){
+                tvTempTomorrow.setText((String)day2.get("temp"));
+                tvTextTomorrow.setText((String)day2.get("textDay"));
+
+                tvTempAfterTomo.setText((String)day3.get("temp"));
+                tvTextAfterTomo.setText((String)day3.get("textDay"));
+
+            } else if (result instanceof String) {
+                if ("2".equals(result)) {
                     Toast.makeText(getActivity(), "服务器错误", Toast.LENGTH_SHORT).show();
-                }else if("3".equals(result)){
+                } else if ("3".equals(result)) {
                     Toast.makeText(getActivity(), "请重新登录", Toast.LENGTH_SHORT).show();
                 }
 
-            }else{
+            } else {
                 Toast.makeText(getActivity(), "服务器错误", Toast.LENGTH_SHORT).show();
             }
         }
     }
-    /**
-     * 自定义适配器
-     */
-    private class Adapter extends BaseAdapter{
-        List<Map<String,Object>> data;
-        public Adapter(List<Map<String,Object>> data){
-            this.data=data;
 
-        }
-        @Override
-        public int getCount() {
-            return data.size();
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            //声明一个ViewHolder
-            WeatherViewHolder viewHolder;
-            if(convertView==null){
-                viewHolder=new WeatherViewHolder();
-                //将转换的View对象存入缓存中
-                convertView=View.inflate(getActivity(),R.layout.item_weather,null);
-                //通过convertView对象来获得控件并将其保存到viewHolder中
-               viewHolder.imWeather= (ImageView) convertView.findViewById(R.id.imWeather);
-               viewHolder.tvDay= (TextView) convertView.findViewById(R.id.tvDay);
-                viewHolder.tvDate= (TextView) convertView.findViewById(R.id.tvDate);
-                viewHolder.tvTemperature= (TextView) convertView.findViewById(R.id.tvTemperature);
-                viewHolder.tvTextDay= (TextView) convertView.findViewById(R.id.tvTextDay);
-                viewHolder.tvUv= (TextView) convertView.findViewById(R.id.tvUv);
-                viewHolder.tvWindDirection= (TextView) convertView.findViewById(R.id.tvWindDirection);
-                viewHolder.tvSuggestion= (TextView) convertView.findViewById(R.id.tvSuggestion);
-
-                convertView.setTag(viewHolder);
-            }else{
-                viewHolder=(WeatherViewHolder)convertView.getTag();
-            }
-            //为控件绑定值
-
-            viewHolder.tvDay.setText((String) data.get(position).get("day"));
-            viewHolder.tvDate.setText((String)data.get(position).get("date"));
-            String s="weather"+data.get(position).get("codeDay");
-            viewHolder.imWeather.setImageResource(getResource(s));
-            viewHolder.tvTemperature.setText((String)data.get(position).get("temp"));
-            viewHolder.tvTextDay.setText((String)data.get(position).get("textDay"));
-            viewHolder.tvWindDirection.setText((String)data.get(position).get("direction"));
-            viewHolder.tvUv.setText((String)data.get(position).get("uv"));
-            viewHolder.tvSuggestion.setText((String)data.get(position).get("suggestion"));
-            switch (position) {
-                case  0:
-                    viewHolder.tvDay.setText("今天");
-                    break;
-                case  1:
-                    viewHolder.tvDay.setText("明天");
-                    break;
-                case  2:
-                    viewHolder.tvDay.setText("后天");
-                    break;
-            }
-
-            return convertView;
-        }
-
-        @Override
-        public Object getItem(int position) {
-            return null;
-        }
-
-        @Override
-        public long getItemId(int position) {
-            return 0;
-        }
-    }
-    public int  getResource(String imageName) {
+    //根据文件名获取图片id
+    public int getResource(String imageName) {
         Context ctx = getActivity().getBaseContext();
         int resId = getResources().getIdentifier(imageName, "mipmap", ctx.getPackageName());
         return resId;
